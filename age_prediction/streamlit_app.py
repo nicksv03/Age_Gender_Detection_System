e32b4270efc85b5379d1c5c5a3ff9e5fb6a2eb7c
Nikhil Verma <dcbs17nikhilverma@gmail.com>
2023-06-26 15:47:53 +0530

    updated files

--- age_prediction/streamlit_app.py
+++ age_prediction/streamlit_app.py
@@ -4,4 +4,22 @@ from PIL import Image
 
 
 def main():
-    st.header('Let me guess your age')
+    st.header("Let me guess, How old are you?")
+    st.write("Upload your photo and I will try to guess your age. (I'm not good at it though ;))")
+    file = st.file_uploader('Upload your photo')
+    if file is not None:
+        image = Image.open(file)
+        image = tf.image.reseize(image, [224,224])
+        image = tf.keras.preprocessing.image.image_to_array(image)
+        image = image / 255.0
+        image = tf.expand_dims(image, axis=0)
+
+        model = tf.keras.models.load_model('model1.h5')
+        age = model.predict(image)
+        
+        st.markdown('### I think you are {} years old'.format(int(age[0][0])))
+        st.write('Curious about how I guessed your age? Check out my [Github] (https://github.com/nicksv03/Age_Gender_Detection_System)')
+
+
+if __name__ == '__main__':
+        main()
